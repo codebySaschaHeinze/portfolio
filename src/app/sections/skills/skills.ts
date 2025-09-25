b30a7fa1d4f3ee9bd48b43406d7a7209f2f70b9f
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-09-25 19:41:21 +0200

    feat: skill-section added and styled. style-fixes in atf-section and about-section

--- src/app/sections/skills/skills.ts
+++ src/app/sections/skills/skills.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Skill } from './../../interfaces/interfaces';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  standalone: true,
   templateUrl: './skills.html',
-  styleUrl: './skills.scss'
+  styleUrl: './skills.scss',
 })
 export class Skills {
-
+  skills: Skill[] = [
+    { name: 'Angular', icon: '/icons/skill-icons/angular-icon.png' },
+    { name: 'TypeScript', icon: '/icons/skill-icons/typescript-icon.png' },
+    { name: 'JavaScript', icon: '/icons/skill-icons/javascript-icon.png' },
+    { name: 'HTML', icon: '/icons/skill-icons/html-icon.png' },
+    { name: 'CSS', icon: '/icons/skill-icons/css-icon.png' },
+    { name: 'Git', icon: '/icons/skill-icons/git-icon.png' },
+    { name: 'API', icon: '/icons/skill-icons/api-icon.png' },
+  ];
 }
