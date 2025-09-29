bfc37435510aff15b450ed1f0880a5d0407017ec
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-09-29 21:56:59 +0200

    feat: project-object added and implemented

--- src/app/interfaces/interfaces.ts
+++ src/app/interfaces/interfaces.ts
@@ -2,3 +2,12 @@ export interface Skill {
   name: string;
   icon: string;
 }
+
+export interface Project {
+  id: number;
+  projectImg: string;
+  title: string;
+  set: string[];
+  description: string;
+  github: string;
+}
--- src/app/sections/works/works.ts
+++ src/app/sections/works/works.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Project } from './../../interfaces/interfaces';
 
 @Component({
   selector: 'app-works',
@@ -6,4 +7,30 @@ import { Component } from '@angular/core';
   templateUrl: './works.html',
   styleUrl: './works.scss',
 })
-export class Works {}
+export class Works {
+  projects: Project[] = [
+    {
+      id: 1,
+      projectImg: 'imgs/elpolloloco.png',
+      title: 'El Pollo Loco',
+      set: ['HTML', 'CSS', 'JavaScript'],
+      description:
+        'El Pollo Loco ist ein kleines Jump-and-Run-Spiel im Browser, bei dem man eine Figur steuert, Münzen und Flaschen sammelt und Hühner als Gegner hat. Am Ende wartet ein großer Endboss, den man mit den gesammelten Flaschen besiegen muss.',
+      github: 'GitHub',
+    },
+
+    {
+      id: 2,
+      projectImg: 'imgs/pokedex.png',
+      title: 'PokeDex',
+      set: ['HTML', 'CSS', 'JavaScript', 'API'],
+      description:
+        'Der Pokédex ist eine Web-App, in der man Pokémon mit Bild, Namen und Details wie Typ oder Fähigkeiten anzeigen kann. Über eine Schnittstelle zur PokéAPI werden die Daten geladen und übersichtlich in Kartenform dargestellt.',
+      github: 'GitHub',
+    },
+  ];
+
+  openGithub(url: string) {
+    window.open(url, '_blank');
+  }
+}
