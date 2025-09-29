a11b6fcd704dd2f3e326f25c18a2468c707aee47
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-09-29 14:54:13 +0200

    fix: content-limitation style bug fixed (100% width)

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,7 @@ import { Header } from './shared/header/header';
 import { Atf } from './sections/atf/atf';
 import { About } from './sections/about/about';
 import { Skills } from './sections/skills/skills';
-import { Work } from './sections/work/work';
+import { Works } from './sections/works/works';
 import { Testimonials } from './sections/testimonials/testimonials';
 import { Contact } from './sections/contact/contact';
 import { Footer } from './shared/footer/footer';
@@ -12,7 +12,7 @@ import { Footer } from './shared/footer/footer';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, Atf, About, Skills, Work, Testimonials, Contact, Footer],
+  imports: [Header, Atf, About, Skills, Works, Testimonials, Contact, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
---
+++ src/app/sections/works/works.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-works',
+  imports: [],
+  templateUrl: './works.html',
+  styleUrl: './works.scss',
+})
+export class Works {}
