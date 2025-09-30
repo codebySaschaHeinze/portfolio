45853dea4f9c29b552ff9c21953d74745ec8095b
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-09-30 12:04:27 +0200

    feat: testimonial-section added (no style yet) "Testimonials"-Object added.

--- src/app/sections/testimonials/testimonials.ts
+++ src/app/sections/testimonials/testimonials.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { Project, Testimonial } from './../../interfaces/interfaces';
 
 @Component({
   selector: 'app-testimonials',
   imports: [],
   templateUrl: './testimonials.html',
-  styleUrl: './testimonials.scss'
+  styleUrl: './testimonials.scss',
 })
 export class Testimonials {
-
+  testimonials: Testimonial[] = [
+    {
+      id: 1,
+      name: 'Daniel Luzius',
+      project: 'Kochwelt',
+      text: 'Das ist ein Platzhalter! Die Zusammenarbeit war angenehm und professionell. Aufgaben wurden zuverlässig umgesetzt und das gemeinsame Ergebnis konnte sich sehen lassen.',
+    },
+    {
+      id: 2,
+      name: 'Patrick Schmidt',
+      project: 'Kochwelt',
+      text: 'Das ist ein Platzhalter! Die Zusammenarbeit war angenehm und professionell. Aufgaben wurden zuverlässig umgesetzt und das gemeinsame Ergebnis konnte sich sehen lassen.',
+    },
+    {
+      id: 3,
+      name: 'Pamela Anderson',
+      project: 'BeachApp',
+      text: 'Das ist ein Platzhalter! Die Zusammenarbeit war angenehm und professionell. Aufgaben wurden zuverlässig umgesetzt und das gemeinsame Ergebnis konnte sich sehen lassen.',
+    },
+  ];
 }
