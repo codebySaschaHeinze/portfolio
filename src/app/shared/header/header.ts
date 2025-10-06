c6ab1287bdbf74f4c94ffa0b9bb778bb48909323
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-10-06 18:40:20 +0200

    chore: working at mediaQuery (not done yet)

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html',
-  styleUrl: './header.scss'
+  styleUrl: './header.scss',
 })
 export class Header {
+  isOverlayOpen = false;
 
+  toggleOverlay() {
+    this.isOverlayOpen = !this.isOverlayOpen;
+  }
+
+  closeOverlay() {
+    this.isOverlayOpen = false;
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeydown(event: KeyboardEvent) {
+    if (event.key === 'Escape' && this.isOverlayOpen) {
+      this.closeOverlay();
+    }
+  }
 }
