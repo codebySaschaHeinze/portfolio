4c369adc7f2d14e5061f7083c0aa0daa07bb158b
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-10-09 20:36:36 +0200

    fix: translation-bug fixed (wrong i18n-folder!)

--- src/app/sections/testimonials/testimonials.ts
+++ src/app/sections/testimonials/testimonials.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { Project, Testimonial } from './../../interfaces/interfaces';
 import { TranslateModule } from '@ngx-translate/core';
+import { Testimonial } from './../../interfaces/interfaces';
 
 @Component({
   selector: 'app-testimonials',
+  standalone: true,
   imports: [TranslateModule],
   templateUrl: './testimonials.html',
   styleUrl: './testimonials.scss',
@@ -13,20 +14,20 @@ export class Testimonials {
     {
       idT: 1,
       nameT: 'Daniel Luzius',
-      project: 'Kochwelt',
-      text: 'Mit Sascha zu arbeiten macht Spaß, weil er Kreativität mit Struktur verbindet. Er behält auch in stressigen Phasen den Überblick und motiviert das Team, das Beste aus jedem Projekt herauszuholen.',
+      projectKey: 'TESTIMONIALS.KOCHWELT',
+      textKey: 'TESTIMONIALS.DANIEL',
     },
     {
       idT: 2,
       nameT: 'Patrick Schmidt',
-      project: 'Kochwelt',
-      text: 'Sascha brachte in Teamarbeiten dank seiner kreativen Impulse, seiner Konzentrationsstärke und seines breiten Wissens einen besonderen Mehrwert ein.',
+      projectKey: 'TESTIMONIALS.KOCHWELT',
+      textKey: 'TESTIMONIALS.PATRICK',
     },
     {
       idT: 3,
       nameT: 'David Hasselhoff',
-      project: 'BeachApp',
-      text: 'Das ist ein Platzhalter! Die Zusammenarbeit war angenehm und professionell. Aufgaben wurden zuverlässig umgesetzt und das gemeinsame Ergebnis konnte sich sehen lassen.',
+      projectKey: 'TESTIMONIALS.BEACHAPP',
+      textKey: 'TESTIMONIALS.DAVID',
     },
   ];
 }
