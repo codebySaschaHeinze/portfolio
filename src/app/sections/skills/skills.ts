2ce8220b95a8ddd06528bb1aea2611666c584b82
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-05-20 13:58:12 +0200

    feat(skills): updated skills

--- src/app/sections/skills/skills.ts
+++ src/app/sections/skills/skills.ts
@@ -11,12 +11,15 @@ import { TranslateModule } from '@ngx-translate/core';
 })
 export class Skills {
   skills: Skill[] = [
-    { nameS: 'Angular', icon: '/icons/skill-icons/angular-icon.png' },
-    { nameS: 'TypeScript', icon: '/icons/skill-icons/typescript-icon.png' },
-    { nameS: 'JavaScript', icon: '/icons/skill-icons/javascript-icon.png' },
     { nameS: 'HTML', icon: '/icons/skill-icons/html-icon.png' },
     { nameS: 'CSS', icon: '/icons/skill-icons/css-icon.png' },
-    { nameS: 'Git', icon: '/icons/skill-icons/git-icon.png' },
+    { nameS: 'JavaScript', icon: '/icons/skill-icons/javascript-icon.png' },
+    { nameS: 'TypeScript', icon: '/icons/skill-icons/typescript-icon.png' },
+    { nameS: 'Angular', icon: '/icons/skill-icons/angular-icon.png' },
+    { nameS: 'Python', icon: '/icons/skill-icons/python-icon.png' },
+    { nameS: 'Django', icon: '/icons/skill-icons/django-icon.png' },
     { nameS: 'API', icon: '/icons/skill-icons/api-icon.png' },
+    { nameS: 'Git', icon: '/icons/skill-icons/git-icon.png' },
+    { nameS: 'Docker', icon: '/icons/skill-icons/docker-icon.png' },
   ];
 }
