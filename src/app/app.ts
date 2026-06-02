33eca85f242161b4a69c2c44f7ff6e1e471e696e
Sascha Heinze <sascha.heinze.dev@gmail.com>
2026-06-02 17:43:13 +0200

    fix: correct burger menu accessibility labels

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
 import { Footer } from './shared/footer/footer';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +9,4 @@ import { TranslateService } from '@ngx-translate/core';
   imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
 })
-export class App {
-  constructor(private t: TranslateService) {
-    this.t.setDefaultLang('de');
-    this.t.use('de');
-  }
-}
+export class App {}
